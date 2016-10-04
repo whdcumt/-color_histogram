3ca2586d88b6b4c4b372957450cb6c42adc69544
whdcumt <wanghongdong0@126.com>
2016-10-04 17:31:47 +0800

    the perfect color histogram

--- test.m
+++ test.m
@@ -2,3 +2,7 @@ p1= calcrgb2hist('1.jpg');
 p2= calcrgb2hist('2.jpg');  
 g = corrcoef(p1 ,p2);  
 fprintf('相关系数为 = %d\n',g(1,2));  
+%前面为计算相关系数的程序
+%后面为将三个通道的直方图曲线绘制到一张图上
+plot(0:255,rhist,'r',0:255,ghist,'g',0:255,bhist,'b')
+axis([0 255,-inf,inf]) %设置横坐标的取值范围
